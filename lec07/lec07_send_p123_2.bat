ad41fb3351615b42aecb9c65c522e4c99e3d0bba
SEO <jpssjjhh@gmail.com>
2026-09-17 11:00:00 +0900

    Add SVG diagram and documentation for Lecture 06 Exercise 07; create batch scripts for sending messages

---
+++ lec07/lec07_send_p123_2.bat
@@ -0,0 +1,7 @@
+@echo off
+chcp 65001 >nul
+set /p EMAIL=받을 지메일 주소를 입력하고 Enter: 
+curl -s -S -X POST http://localhost:5678/webhook-test/day11-intake -H "Content-Type: application/json" -d "{\"name\":\"홍길동\",\"email\":\"%EMAIL%\",\"message\":\"지난번 문의 답을 못 받았어요\"}"
+echo.
+echo (위에 JSON이 보이면 n8n이 받은 것)
+pause
